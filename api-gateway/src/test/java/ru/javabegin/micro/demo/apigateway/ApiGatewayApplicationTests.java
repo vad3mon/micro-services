356334fd60ac5fada71ba27d094442ad53b94728
vad3mon <gg-gg13@bk.ru>
2024-10-29 13:09:18 +0300

    Создание микросервисов, eureka server, client, api-gateway

---
+++ api-gateway/src/test/java/ru/javabegin/micro/demo/apigateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,14 @@
+package ru.javabegin.micro.demo.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,6 @@
+rootProject.name = "spring-micro-demo"
+
+includeBuild ("eureka-server")
+includeBuild ("eureka-client")
+includeBuild ("eureka-client-2")
+includeBuild ("api-gateway")
